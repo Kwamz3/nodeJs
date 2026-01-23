8d940df07b8f7acb29ccdf99324f6b2cc032f587
Kwamz3 <charlie18appiah@gmail.com>
2026-01-23 18:30:15 +0000

    Refactor response handling in server to improve readability and maintainability

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const server = http.createServer((req, res) => {
 
     // you can send a response body through the "res.end"
     // res.write("Hello World!")
-   
-    res.setHeader('Content-type', 'text/html')
-    res.end('<h1>Hello World!</h1>')
 
+    console.log(req.url);
+    console.log(req.method);
+    
+    // res.setHeader('Content-type', 'text/html')
+    res.writeHead(200, {'Content-type': 'text/html'})
+    res.end('<h1>Hello World!</h1>')
+    
     // you can use "writeHead" to pass the status code and the Context type at a go
     // res.writeHead(500, { 'Content-Type': 'application/json'});
     // res.end(JSON.stringify({ message: 'Server Error' }))
