e1278b5cd6e81c66c5a3f136b6e06899d74ccb4f
Kwamz3 <charlie18appiah@gmail.com>
2026-01-24 11:31:49 +0000

    Refactor GET request handling in server.js to prepare for dynamic file serving

--- server.js
+++ server.js
@@ -51,15 +51,13 @@ const server = http.createServer((req, res) => {
     
     try {
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, {'content-type': 'text/html'});
-                res.end('<h1>Homepage</h1>')
+                
             } else if (req.url === '/about') {
-                res.writeHead(200, {'content-type': 'text/html'});
-                res.end('<h1>About</h1>')
+                
             } else {
-                res.writeHead(404, {'content-type': 'text/html'});
-                res.end('<h1>Not Found</h1>')
+               
             } 
         } else {
             throw new Error('Method not allowed')
