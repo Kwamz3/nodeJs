65d3af01be28bfbf6776dd368608c8ce8f64f700
Kwamz3 <charlie18appiah@gmail.com>
2026-01-24 08:44:33 +0000

    Refactor request handling to include error handling for unsupported methods

--- server.js
+++ server.js
@@ -17,16 +17,27 @@ const server = http.createServer((req, res) => {
     // res.writeHead(500, { 'Content-Type': 'application/json'});
     // res.end(JSON.stringify({ message: 'Server Error' }))
 
-    if (req.url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end('<h1>Homepage</h1>')
-    } else if (req.url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end('<h1>About</h1>')
-    } else {
-        res.writeHead(404, {'content-type': 'text/html'});
-        res.end('<h1>Not Found</h1>')
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.end('<h1>Homepage</h1>')
+            } else if (req.url === '/about') {
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.end('<h1>About</h1>')
+            } else {
+                res.writeHead(404, {'content-type': 'text/html'});
+                res.end('<h1>Not Found</h1>')
+            } 
+        } else {
+            throw new Error('Method not allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, {'content-type': 'text/plain'});
+        res.end('Server Error')
     }
+
+
 })
 
 server.listen(PORT, () => {
