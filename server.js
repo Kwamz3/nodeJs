4d20afcac2409d0c5a453d33f388ada57ed00acb
Kwamz3 <charlie18appiah@gmail.com>
2026-01-24 09:17:07 +0000

    Remove commented-out code for request handling in server.js

--- server.js
+++ server.js
@@ -27,6 +27,28 @@ const server = http.createServer((req, res) => {
     // res.writeHead(500, { 'Content-Type': 'application/json'});
     // res.end(JSON.stringify({ message: 'Server Error' }))
 
+    // try {
+    //     if (req.method === 'GET') {
+    //         if (req.url === '/') {
+    //             res.writeHead(200, {'content-type': 'text/html'});
+    //             res.end('<h1>Homepage</h1>')
+    //         } else if (req.url === '/about') {
+    //             res.writeHead(200, {'content-type': 'text/html'});
+    //             res.end('<h1>About</h1>')
+    //         } else {
+    //             res.writeHead(404, {'content-type': 'text/html'});
+    //             res.end('<h1>Not Found</h1>')
+    //         } 
+    //     } else {
+    //         throw new Error('Method not allowed')
+    //     }
+    // } catch (error) {
+    //     res.writeHead(500, {'content-type': 'text/plain'});
+    //     res.end('Server Error')
+    // }
+    
+    // Using the __filename object to load the html pages
+    
     try {
         if (req.method === 'GET') {
             if (req.url === '/') {
