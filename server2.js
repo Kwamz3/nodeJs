2349ad0e570373e91d1178b031b0c2a797ee4ccd
Kwamz3 <charlie18appiah@gmail.com>
2026-02-06 03:44:16 +0000

    Fix GET request handling for specific user data in server2.js

--- server2.js
+++ server2.js
@@ -15,8 +15,13 @@ const server = createServer((req, res) => {
         res.setHeader('Content-type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
-    } else if (req.url.match(/^\/api\/users\/([0-9]+)/)) {
-        
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        const user = users.find((users) => { users.id === id });
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.write(JSON.stringify({ id: 1, name: "John Dope" }));
+        res.end();
     } else {
         res.setHeader('Content-type', 'application/json');
         res.statusCode = 404;
