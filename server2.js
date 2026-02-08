bbcc8dae7c23b5ce7a33997bf4c7cf43a9e8048d
Kwamz3 <charlie18appiah@gmail.com>
2026-02-08 12:27:55 +0000

    Fix user retrieval logic in GET request for specific user data in server2.js

--- server2.js
+++ server2.js
@@ -17,20 +17,17 @@ const server = createServer((req, res) => {
         res.end();
     } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[3];
-        const user = users.find((user) => { user.id === parseInt(id) });
+        const user = users.find((user) => user.id === parseInt(id));
+        res.setHeader('Content-Type', 'application/json');
 
         if (user) {
-        res.setHeader('Content-Type', 'application/json');
         res.statusCode = 200;
         res.write(JSON.stringify(user));
-        res.end();
         } else {
-        res.setHeader('Content-type', 'application/json');
         res.statusCode = 404;
         res.write(JSON.stringify({ message: 'User not Found' }));
-        res.end();
         }
-       
+        res.end();
     } else {
         res.setHeader('Content-type', 'application/json');
         res.statusCode = 404;
